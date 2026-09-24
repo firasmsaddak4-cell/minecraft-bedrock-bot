9eba7ddf7e379eb0c8b13b589d670a9e6c64a229
firasmsaddak4-cell <firasmsaddak4@gmail.com>
2026-09-24 13:19:56 +0100

    Fix username typo in bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ function start() {
   const client = bedrock.createClient({
     host: 'ssafe77.aternos.me',
     port: 54809,
-    username: 'safe7bott',
+    username: 'safe7bot',
     offline: true,
     skipPing: true,
     version: '1.21.130', // اتركه كما نجح معك
